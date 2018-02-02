d1a76ad7403caf64d02e3e14b3a596c22e725dde
Sebastian Ertel <sebastian.ertel@gmail.com>
2018-02-02 20:15:03 +0100

    mock db for test case.

--- test/KvserviceTest.hs
+++ test/KvserviceTest.hs
@@ -1,7 +1,29 @@
+{-# LANGUAGE InstanceSigs, FlexibleInstances #-}
 
 import Test.HUnit hiding (State)
 import Test.Framework
 import Test.Framework.Providers.HUnit
+
+import qualified Data.Text.Lazy         as T
+import qualified Data.HashMap.Strict    as HM
+import           Data.Maybe
+
+import qualified DB_Iface               as DB
+
+type MockDB = HM.HashMap T.Text T.Text
+
+instance DB.DB_Iface MockDB where
+  get :: MockDB -> T.Text -> IO T.Text
+  get db key = return $ fromJust $ HM.lookup key db
+
+  put :: MockDB -> T.Text -> T.Text -> IO T.Text
+  put db key value =
+    let
+      convert = \x -> case x of { (Just p) -> p; Nothing -> T.empty }
+      prev = (convert . HM.lookup key) db
+      _ = HM.insert key value db
+    in
+      return prev
 
 simpleTest :: Assertion
 simpleTest = do
@@ -10,7 +32,6 @@ simpleTest = do
   -- assertEqual "state was wrong." [2,3] state
   return undefined
 
-none-sense
 
 main :: IO ()
 main = defaultMainWithOpts
