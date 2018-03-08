ab221a822dfd2096622d4c1689c247858663f3fd
Sebastian Ertel <sebastian.ertel@gmail.com>
2018-03-08 11:45:07 +0100

    configuring execution via command line arguments and enforcing the benchmark suite to be executed sequentially.

--- README.md
+++ README.md
@@ -17,3 +17,15 @@ To re-generate the code for the Thrift part navigate to the `thrift` directory a
 thrift -r --gen hs kvservice.thrift
 thrift -r --gen hs db.thrift
 ```
+
+Correctness Tests:
+```
+stack test
+stack test --ta=-sct
+stack test --ta=-suite=ct
+```
+
+Micro Benchmark (need to/should be executed sequentially):
+```
+stack test --ta="-smb -eseq"
+```
--- test/KvserviceTest.hs
+++ test/KvserviceTest.hs
@@ -4,29 +4,62 @@ import Test.HUnit hiding (State)
 import Test.Framework
 import Test.Framework.Providers.HUnit
 
+import Options.Applicative
+import Data.Semigroup ((<>))
+
 import qualified Kvstore.KeyValueService   as KVS
 import qualified Kvstore.Ohua.FBM.KeyValueService   as KVSOhuaFBM
 
-import CorrectnessTests (suite)
--- import Microbenchmark (suite)
+import qualified CorrectnessTests as CT (suite)
+import qualified Microbenchmark as MB (suite)
 
-versions =
+suite arg = case arg of
+  "mb" -> MB.suite
+  _ -> CT.suite
+
+versions s =
   [
-      let ?execRequests = KVS.execRequestsCoarse            in testGroup "\nCoarse-grained imperative version" $ suite "coarse-grained imperative"
+      let ?execRequests = KVS.execRequestsCoarse            in testGroup "\nCoarse-grained imperative version" $ suite s "coarse-grained imperative"
   ,
-      let ?execRequests = KVS.execRequestsFine              in testGroup "\nFine-grained imperative version" $ suite "fine-grained imperative"
+      let ?execRequests = KVS.execRequestsFine              in testGroup "\nFine-grained imperative version" $ suite s "fine-grained imperative"
   ,
-      let ?execRequests = KVS.execRequestsFuncImp           in testGroup "\nFunctional-imperative version" $ suite "functional-imperative"
+      let ?execRequests = KVS.execRequestsFuncImp           in testGroup "\nFunctional-imperative version" $ suite s "functional-imperative"
   ,
-      let ?execRequests = KVS.execRequestsFunctional        in testGroup "\nPurely functional version" $ suite "purely functional"
+      let ?execRequests = KVS.execRequestsFunctional        in testGroup "\nPurely functional version" $ suite s "purely functional"
   ,
-      let ?execRequests = KVSOhuaFBM.execRequestsFunctional in testGroup "\nOhua - FBM version" $ suite "Ohua - FBM"
+      let ?execRequests = KVSOhuaFBM.execRequestsFunctional in testGroup "\nOhua - FBM version" $ suite s "Ohua - FBM"
   ]
 
-runSuite = defaultMainWithOpts versions mempty
+runSuite s = defaultMainWithOpts (versions s) mempty
 
-runSuiteSequentially = defaultMainWithOpts [mutuallyExclusive $ testGroup "\nSequential run" versions] mempty
+runSuiteSequentially s = defaultMainWithOpts [mutuallyExclusive $ testGroup "\nSequential run" (versions s)] mempty
+
+data Config = Config String String
+
+config :: Parser Config
+config = Config
+      <$> strOption
+          ( short 's'
+         <> long "suite"
+         <> metavar "SUITE"
+         <> help "Test suite to be executed."
+         <> showDefault
+         <> value "ct" )
+      <*> strOption
+          ( short 'e'
+         <> long "execution"
+         <> metavar "EXECUTION"
+         <> help "Execution mode of the test suite."
+         <> showDefault
+         <> value "par" )
 
 main :: IO ()
 -- main = runSuite
-main = runSuiteSequentially
+-- main = runSuiteSequentially
+main = do
+  options <- execParser (info (config <**> helper) (fullDesc <> progDesc "A micro service for key-value storage that automatically replicates data across multiple DBs and keeps a cache for fast access."
+                                                             <> header "KVStore"))
+  case options of
+    (Config s "seq") -> runSuiteSequentially s
+    (Config s "par") -> runSuite s
+    (Config s _) -> runSuite s
